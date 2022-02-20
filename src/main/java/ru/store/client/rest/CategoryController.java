8e05183185f15db2938e466d42781ecf0a59e338
MikhailSbuchelov <mikivolkoff@ya.ru>
2022-02-20 10:53:39 +0300

    up lib

---
+++ src/main/java/ru/store/client/rest/CategoryController.java
@@ -0,0 +1,49 @@
+package ru.store.client.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.api.category.api.CategoryGateway;
+import ru.gb.api.category.dto.CategoryDto;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+    private CategoryGateway categoryGateway;
+
+    @GetMapping
+    public List<CategoryDto> getCategoryList() {
+        return categoryGateway.getCategoryList();
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<?> getCategory(@PathVariable("categoryId") Long id) {
+        return categoryGateway.getCategory(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> handlePost(@Validated @RequestBody CategoryDto categoryDto) {
+        categoryGateway.handlePost(categoryDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<?> handleUpdate(@PathVariable("categoryId") Long id,
+                                          @Validated @RequestBody CategoryDto categoryDto) {
+        categoryDto.setCategoryId(id);
+        categoryGateway.handleUpdate(id, categoryDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("categoryId") Long id) {
+        categoryGateway.deleteById(id);
+    }
+
+}
